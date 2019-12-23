07ebe4bcd772543cab5f9265a4ef061e70865c11
Yohan <gentlebae@gmail.com>
2019-12-23 15:23:21 -0500

    Updates

---
+++ precache-manifest.c3358c1e1189fda9120baaba15707977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ca3a29027be073ed21033f679ddae8",
+    "url": "/react_tutorial/index.html"
+  },
+  {
+    "revision": "28503420e6a27902e153",
+    "url": "/react_tutorial/static/js/2.2943ad29.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react_tutorial/static/js/2.2943ad29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc8bde045132c9d75930",
+    "url": "/react_tutorial/static/js/main.cbb7a5ed.chunk.js"
+  },
+  {
+    "revision": "5465a863cb8ac074ffe4",
+    "url": "/react_tutorial/static/js/runtime-main.c44efd8f.js"
+  }
+]);
